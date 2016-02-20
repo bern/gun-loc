9e4f116871f166a86566dad836a930f5e1004ba4
bern422 <bern422@gmail.com>
2016-02-20 09:33:55 -0600

    added routes

--- public/server.js
+++ public/server.js
@@ -1,12 +1,12 @@
-var express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
-
-//Specify a port
-var port = process.env.port || 8080;
-
-//Serve up files in public folder
-app.use('/', express.static(__dirname + '/public'));
-
-//Start up the website
-app.listen(port);
-console.log('Listening on port: ', port);
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+var routes = require("./routes/routes.js")(app);
+ 
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
