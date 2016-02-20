d4a57011dcb5a3dee0c8fff8bac5bb70b0f13ec9
faizanv <faizan.virani.44@gmail.com>
2016-02-20 17:20:20 -0600

    Most CRUD and texting routes

---
+++ models/Hit.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var HitSchema = new mongoose.Schema({
+  hit_at: { type: Date, default: Date.now },
+  gun_id: {type: mongoose.Schema.Types.ObjectId, ref: "Gun"}
+});
+
+module.exports = mongoose.model('Hit', HitSchema);
