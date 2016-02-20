b9b902303cf82c41df6f3639bc25738de40c53e9
bern422 <bern422@gmail.com>
2016-02-20 00:40:36 -0600

    what am i doing

---
+++ controllers/musicians.js
@@ -0,0 +1,12 @@
+exports.findAll = function(req, res) {
+  res.send([{
+    "id": 1,
+    "name": "Max",
+    "band": "Maximum Pain",
+    "instrument": "guitar"
+  }]);
+};
+exports.findById = function() {}
+exports.add = function() {}
+exports.update = function() {}
+exports.delete = function() {}
