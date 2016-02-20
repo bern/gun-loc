a6284861558bad9f0241bc7fd0f6c4d5a4e0b37d
bern422 <bern422@gmail.com>
2016-02-20 01:11:41 -0600

    round two

--- routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-  var musicians = require('./controllers/musicians');
-  app.get('/musicians', musicians.findAll);
-  app.get('/musicians/:id', musicians.findById);
-  app.post('/musicians', musicians.add);
-  app.put('/musicians/:id', musicians.update);
-  app.delete('/musicians/:id', musicians.delete);
-}
