230e45593c80a6e2cdde6d613b67267a218be34c
bern422 <bern422@gmail.com>
2016-02-20 01:10:55 -0500

    rewrote in express

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('waddup nerds\n');
-}).listen(port);
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+  res.send('#yolo\n');
+});
+app.listen(3001);
+console.log('Listening on port 3001...');
