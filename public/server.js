9e899d616440f119368cc53b7d369e80ad3cbd5b
bern422 <bern422@gmail.com>
2016-02-20 09:27:27 -0600

    jesus

---
+++ public/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+//Specify a port
+var port = process.env.port || 8080;
+
+//Serve up files in public folder
+app.use('/', express.static(__dirname + '/public'));
+
+//Start up the website
+app.listen(port);
+console.log('Listening on port: ', port);
