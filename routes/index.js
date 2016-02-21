2f59dcdba16033eea1add58168b7d7e65c8c5780
faizanv <faizan.virani.44@gmail.com>
2016-02-21 00:53:58 -0600

    Finished most routes working on front end connection now

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ var request = require('superagent');
 var Hit = require('../models/Hit.js');
 var Gun = require('../models/Gun.js');
 var User = require('../models/User.js');
+var Hit = require('../models/FakeHit.js');
+
 
 // Twilio Credentials
 var accountSid = 'AC544ce6ea7b29c8e2dea462fa0a21de17';
@@ -29,8 +31,8 @@ router.get('/hit', function(req,res,next) {
     } else {
       res.json(hits);
     }
-  })
-})
+  });
+});
 
 router.post('/hit', function(req, res, next) {
   Gun.findOne({gun_num: 0}, function(err, gun) {
@@ -42,12 +44,50 @@ router.post('/hit', function(req, res, next) {
         if (err) {
           res.send(err);
         } else {
+          text();
+          toggleStatus(function(something) {
+            console.log(something);
+          });
           res.json(hit);
         }
       });
     }
   });
 });
+
+router.get('/reset', function(req, res, next) {
+  toggleStatus(function(something) {
+    res.json(something);
+  });
+});
+
+router.post('/fakehit', function(req, res, next) {
+  Gun.findOne({gun_num: 0}, function(err, gun) {
+    if (err) {
+      res.send(err);
+    } else {
+      var newHit = new FakeHit({gun_id: gun._id, hit_at: req.body.hit_at});
+      newHit.save(function(err, hit) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json(hit);
+        }
+      });
+    }
+  });
+});
+
+router.get('/fakehit', function(req,res,next) {
+  FakeHit.find(function(err, hits) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(hits);
+    }
+  });
+});
+
 
 router.post('/gun', function(req, res, next) {
   var newGun = new Gun(req.body);
@@ -68,23 +108,23 @@ router.get('/gun', function(req, res, next) {
       res.json(guns);
     }
   });
-})
-
-router.get('/text', function(req, res, next) {
-  client.messages.create({
-    to: "+17274224360",
-    from: "+13057832802",
-    body: "Somebody has touched your gun"
-    // mediaUrl: "http://farm2.static.flickr.com/1075/1404618563_3ed9a44a3a.jpg",
-  }, function(err, message) {
-      if (err) {
-        res.send(err);
-      } else {
-        console.log(message.sid);
-        res.send("Text sent");
-      }
-  });
 });
+
+// router.get('/text', function(req, res, next) {
+//   client.messages.create({
+//     to: "+17274224360",
+//     from: "+13057832802",
+//     body: "Somebody has touched your gun"
+//     // mediaUrl: "http://farm2.static.flickr.com/1075/1404618563_3ed9a44a3a.jpg",
+//   }, function(err, message) {
+//       if (err) {
+//         res.send(err);
+//       } else {
+//         console.log(message.sid);
+//         res.send("Text sent");
+//       }
+//   });
+// });
 
 router.post('/user', function(req,res,next) {
   var newUser = new User(req.body);
@@ -106,5 +146,39 @@ router.get('/user', function(req, res, next) {
     }
   });
 });
+
+function text() {
+  client.messages.create({
+    to: "+17274224360",
+    from: "+13057832802",
+    body: "Somebody has touched your gun"
+    // mediaUrl: "http://farm2.static.flickr.com/1075/1404618563_3ed9a44a3a.jpg",
+  }, function(err, message) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(message.sid);
+        console.log("Text sent");
+      }
+  });
+}
+//active, alert, off
+function toggleStatus(callback) {
+  Gun.findOne({gun_num: 0}, function(err, gun) {
+      var status = gun.status;
+      if (status.toLowerCase() == 'active') {
+        gun.status = 'alert';
+      } else if (status.toLowerCase() == 'alert') {
+        gun.status = 'active';
+      }
+      gun.save(function(err, gun) {
+        if (err) {
+          callback(err);
+        } else {
+          callback(gun);
+        }
+      });
+  });
+}
 
 module.exports = router;
