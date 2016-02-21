1cad1afee2dcd5a5898b5d4e3a1e79be611ef9c5
Troy Gittelmacher <tgittelmacher@github.com>
2016-02-21 05:19:41 -0500

    real time db updates

--- public/javascripts/module.js
+++ public/javascripts/module.js
@@ -1,6 +1,6 @@
 var module = angular.module('app', []);
 
-module.controller('appController', ['$scope', function($scope) {
+module.controller('appController', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
     
     $scope.data = {
       "username": "johndoe",
@@ -44,9 +44,72 @@ module.controller('appController', ['$scope', function($scope) {
           }
       ]
     };
+    
+    $scope.guns = [
+          {
+              "acquisition_date": "20 Feb, 2016",
+              "firearm_type": "handgun",
+              "manufacturer": "glock",
+              "model": "17",
+              "caliber_guage": "9mm",
+              "serial_num": "12345678",
+              "status": "active"
+          },
+          {
+              "acquisition_date": "18 Feb, 2016",
+              "firearm_type": "shotgun",
+              "manufacturer": "benelli",
+              "model": "cordoba",
+              "caliber_guage": "12 guage",
+              "serial_num": "87654321",
+              "status": "alert"
+          },
+          {
+              "acquisition_date": "16 Feb, 2016",
+              "firearm_type": "rifle",
+              "manufacturer": "browning",
+              "model": "x-bolt medallion",
+              "caliber_guage": "lol idk",
+              "serial_num": "13247586",
+              "status": "off"
+          }
+      ];
 
     $scope.enableGun = function(gun) {
         gun.status="active";
         //tell database this
     }
+    
+    $scope.getData = function() {
+        console.log("Fetching data...");
+        $http({
+          method: 'GET',
+          url: '/gun'
+        }).then(function successCallback(response) {
+            console.log("Success!");
+            console.log(response);
+            $scope.guns = response.data;
+          }, function errorCallback(response) {
+            console.log("Oops...");
+            console.log(response);
+        });
+    }
+    
+    $scope.pollDB = function (message) {
+        
+        var intervalPeriod = 5000;
+        
+        var rep = $interval(function () {
+            $scope.getData();
+        }, intervalPeriod);
+        
+    }
+    
+    var init = function() {
+        $scope.getData();
+        $scope.pollDB();
+    };
+    
+    init();
+    
 }]);
